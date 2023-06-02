df1effe01141d32ca475bdb90d30f0226f9ccd4a
Jagadeesh Kanna J <100136974+Jaga2101@users.noreply.github.com>
2023-06-02 10:45:18 +0530

    Add files via upload

---
+++ dsh.js
@@ -0,0 +1,46 @@
+function dash() {
+    window.location = "dash.html";
+}
+function emp() {
+    window.location = "emp.html";
+}
+function sup(){
+    window.location = "sup.html";
+}
+function firm() {
+    window.location = "firm.html";
+}
+function cat() {
+    window.location = "cat.html";
+}
+function ty() {
+    window.location = "ty.html";
+}
+function pro() {
+    window.location = "pro.html";
+}
+function st() {
+    window.location = "stock_details.html";
+}
+function pro_list() {
+    window.location = "pro_list.html";
+}
+function bill() {
+    window.location = "bill.html";
+}
+function sbill() {
+    window.location = "sbill.html";
+}
+function sb() {
+    window.location = "sb.html";
+}
+function show_seller_bill() {
+    window.location = "show_seller_bill.html";
+}
+function inlo() {
+    window.location = "index.html";
+}
+//
+function addemp() {
+    window.location = "emp_add.html";
+}
